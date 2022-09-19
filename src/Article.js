dcac64d1ebe762a173c590d35eda30087c77454f
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-09-19 15:19:58 +0300

    done with assignment

--- src/Article.js
+++ src/Article.js
@@ -5,5 +5,15 @@ function Article() {
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
-
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
+function App() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  ); 
+}
 export default Article;
